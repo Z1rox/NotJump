68747ecdd7e8d00565382819b33a135c1a319e82
Z1rox <danilichberkut@gmail.com>
2024-10-07 18:37:11 +0300

    update1.2.3.2

--- username.js
+++ username.js
@@ -1,5 +1,20 @@
 const tg = window.Telegram.WebApp;
 tg.expand();
+tg.setBackgroundColor("#000000");
+tg.setHeadorColor("#000000");
+const canvas = document.getElementById('board');
+const ctx = canvas.getContext('2d');
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+}
+
+window.onload = function() {
+    resizeCanvas();
+    telegramWebApp.isLoaded();
+};
+
+window.onresize = resizeCanvas;
 
 let username = tg.initDataUnsafe.user.username;
-tg.setBackgroundColor("#000000");
