ef7660ecfb4abd1b2439f72c243c8c1ac3768c8d
Z1rox <danilichberkut@gmail.com>
2024-11-10 00:01:02 +0200

    update3.0.0.0

--- username.js
+++ username.js
@@ -1,23 +1,16 @@
 const tg = Telegram.WebApp;
 
-// Функция для проверки, является ли устройство мобильным
 function isMobileDevice() {
-    // Проверка на мобильные устройства (Android, iPhone, iPad, iPod)
     return /Android|iPhone|iPad|iPod|Opera Mini|IEMobile|Mobile|BlackBerry/i.test(navigator.userAgent);
 }
 
-// Функция для проверки, является ли устройство MacBook или компьютером
 function isNonMobileAppleDevice() {
-    // Проверка на устройства с macOS (MacBook, iMac и т.д.)
     return /Macintosh|Mac OS/i.test(navigator.userAgent) && !/iPhone|iPad/i.test(navigator.userAgent);
 }
 
-// Проверка платформы и блокировка доступа на MacBook, компьютерах и других неподдерживаемых устройствах
 if (tg.platform === "desktop" || !isMobileDevice() || isNonMobileAppleDevice()) {
-    // Выводим сообщение и блокируем приложение для компьютеров, MacBook и других неподдерживаемых устройств
     document.body.innerHTML = "<h2>Sorry, but you need to use a mobile phone or tablet to access this app.</h2>";
 } else {
-    // Код для мобильных устройств
     tg.expand();
     tg.setHeaderColor("#000000");
     tg.setBackgroundColor("#000000");
@@ -41,5 +34,4 @@ if (tg.platform === "desktop" || !isMobileDevice() || isNonMobileAppleDevice()) 
         updateUsername();
     });
 
-    // Продолжение вашего основного кода...
 }
